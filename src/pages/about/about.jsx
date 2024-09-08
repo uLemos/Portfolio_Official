825ff2e4d5e0b2467d4710b04c143c0400cd1656
uLemos <fsnvag12@gmail.com>
2024-09-08 18:21:24 -0300

    :memo: Finalized About

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -4,23 +4,50 @@ import "./about.css";
 const About = () => {
   return (
     <>
+    <div className="containerAbout">
       <section className="lineDiv">
         <div className="lineDiv__div" />
       </section>
+      <h1 className="about__title">Sobre</h1>
       <section className="about">
         <div className="about__leftImages">
           <div className="about__icons">
             <img src="../../src/assets/icons.png" className="img__icon"/>
-            <span className="about__iconsDesc">Tecnologias e Frameworks</span>
+            <div className="aboutAutoSlide">
+              <div className="aboutAutoSlide__items">
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angularjs/angularjs-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/json/json-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/xml/xml-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/yaml/yaml-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sqldeveloper/sqldeveloper-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azuresqldatabase/azuresqldatabase-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/apache/apache-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dotnetcore/dotnetcore-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dot-net/dot-net-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/spring/spring-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/oracle/oracle-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azure/azure-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-original-wordmark.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/openapi/openapi-original.svg" />
+              </div>
+            </div>
           </div>
           <div className="about__imgPikachu">
             <img src="../../src/assets/pikachuOlhandoPraCima.png" className="img__Pikachu"/>
           </div>
         </div>
         <div className="about__rightInfo">
-          <div className="cardDecoration" />
+          <div className="about__card">
+            <div className="cardDecoration" />
+          </div>
         </div>
       </section>
+    </div>
     </>
   )
 }
