52c684949e3b2250bcec441494d4ad0d23b5cd4b
uLemos <fsnvag12@gmail.com>
2024-09-10 01:23:21 -0300

    :memo: Finaly section from about

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./about.css";
 
+const SvgArrow = () => {
+  return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="ul__svg">
+          <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm4.28 10.28a.75.75 0 0 0 0-1.06l-3-3a.75.75 0 1 0-1.06 1.06l1.72 1.72H8.25a.75.75 0 0 0 0 1.5h5.69l-1.72 1.72a.75.75 0 1 0 1.06 1.06l3-3Z" clip-rule="evenodd" />
+        </svg>;
+}
+
 const About = () => {
   return (
     <>
@@ -11,7 +17,6 @@ const About = () => {
       <section className="about">
         <div className="about__leftImages">
           <div className="about__icons">
-            
             <div className="about__card">
               <div className="cardDecoration" />
               <img src="../../src/assets/perfilF.png" className="card__photo" />
@@ -20,8 +25,6 @@ const About = () => {
                 <span className="card__subTitlePhoto">Full-Stack Developer</span>
               </div>
             </div>
-
-            {/* <img src="../../src/assets/icons.png" className="img__icon"/> */}
             <div className="aboutAutoSlide">
               <div className="aboutAutoSlide__items">
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angularjs/angularjs-original.svg" />
@@ -51,26 +54,28 @@ const About = () => {
           </div>
         </div>
         <div className="about__rightInfo">
-         <span className="aboutRight__subTitle">Soft/Hard - Skills</span>
-         <h1 className="aboutRight__title">Conceitos</h1>
+          <div className="container_Title">
+            <span className="aboutRight__subTitle">Soft/Hard - Skills</span>
+            <h1 className="aboutRight__title">Conceitos</h1>
+          </div>
          <ul className="aboutRight__ul">
           <li className="ul__li">
-            <a href="" className="li__a">Arquitetura Limpa</a>
+            <SvgArrow/> <a href="" className="li__a">Arquitetura Limpa;</a>
           </li>
           <li className="ul__li">
-            <a href="" className="li__a">DDD e TDD</a>
+            <SvgArrow/> <a href="" className="li__a">DDD e TDD;</a>
           </li>
           <li className="ul__li">
-            <a href="" className="li__a">RESTFul API`s</a>
+            <SvgArrow/> <a href="" className="li__a">RESTFul API`s;</a>
           </li>
           <li className="ul__li">
-            <a href="" className="li__a">Mensageria RabbitMq/Kafka</a>
+            <SvgArrow/> <a href="" className="li__a">Mensageria RabbitMq/Kafka;</a>
           </li>
           <li className="ul__li">
-            <a href="" className="li__a">Docker</a>
+            <SvgArrow/> <a href="" className="li__a">Docker;</a>
           </li>
           <li className="ul__li">
-            <a href="" className="li__a">CI/CD</a>
+            <SvgArrow/> <a href="" className="li__a">CI/CD.</a>
           </li>
          </ul>
         </div> 
