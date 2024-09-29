dad3a02eb31e554742a8a26a43d8f4202c400a86
uLemos <fsnvag12@gmail.com>
2024-09-29 12:39:01 -0300

    :memo: simple method from send e-mail with useState()

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -1,30 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./contact.css";
 import LineDiv from "../../components/lineDiv/lineDiv";
 import LineFromTitle from "../../components/UI/LineFromTitle";
-import { useForm } from "react-hook-form";
 
 const Contact = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm();
-
-  const onSubmit = (data) => {
-    alert(JSON.stringify(data));
+  const [obj, setObj] = useState({
+    nameComplete: "",
+    email: "",
+    tel: "",
+    message: ""
+  });
+  
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(obj);
   }
 
-  console.log("RENDER");
-
-  return (
+    return (
     <>
       <LineDiv />
       <div className="contact" id="contact">
         <LineFromTitle title={"Contato"}/>
-        <form onSubmit={handleSubmit((data) => console.log(data))} className="contact__form">
+        <form onSubmit={handleSubmit} className="contact__form">
+
           <div className="contact__infosUser">
+
             <div className="contact__img">
               <div className="contact__line" />
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="contact__svg">
@@ -33,28 +33,48 @@ const Contact = () => {
               <div className="contact__line" />
             </div>
 
-            <label htmlFor="">Nome</label>
+            <label htmlFor="nameComplete">Nome</label>
             <input 
-              className={errors?.name && "input-error"}
               type="text"
-              placeholder="Informe seu nome" />
-              {...register('name', {required: true})} 
-              
-            <label {...register('email')} >E-mail</label>
-            <input type="email" placeholder="Informe seu e-mail" />
+              value={obj.nameComplete}
+              placeholder="Informe seu nome" 
+              onChange={ data => setObj({ ...obj, nameComplete: data.target.value })}
+            />
+            
+            <label htmlFor="email">E-mail</label>
+            <input 
+              type="email"
+              value={obj.email}
+              placeholder="Informe seu e-mail" 
+              onChange={ data => setObj({ ...obj, email: data.target.value }) }
+            />
 
-            <label {...register('phone')} >Celular</label>
-            <input type="tel" placeholder="(00) 00000-0000" />
+            <label htmlFor="tel">Celular</label>
+            <input 
+              type="text"
+              value={obj.tel}
+              placeholder="(00) 00000-0000" 
+              onChange={ data => setObj({ ...obj, tel: data.target.value }) }
+            />
 
           </div>
+
           <div className="contact__message">
+
             <div className="message">
-              <label htmlFor="">Mensagem</label>
-              <textarea {...register('mensagem')} placeholder="Informe sua mensagem"/>
+              <label htmlFor="message">Mensagem</label>
+              <textarea 
+                type="textarea"
+                value={obj.message}
+                placeholder="Informe sua mensagem"
+                onChange={ data => setObj({ ...obj, message: data.target.value }) }
+              />
             </div>
+
             <div className="contact__btn">
               <button type="submit" className="message__btn">Enviar e-mail</button>
             </div>
+
           </div>
         </form>
       </div>
